a69388427db824e4a1ed13e7961fa96af62da503
DDorka02 <dobszaydorka@gmail.com>
2024-02-23 13:43:17 +0100

    hatlom

--- src/babosjatek/Rendezesek.java
+++ src/babosjatek/Rendezesek.java
@@ -11,9 +11,9 @@ public class Rendezesek {
         System.out.println("Kérek egy számot(1-2):");
         int c =sc.nextInt();
         while (3<c){
-        if (3<c){
-            System.out.println("Hibás. Kérek egy számot(1-2):");
-            c =sc.nextInt();}
+            if (3<c){
+                System.out.println("Hibás. Kérek egy számot(1-2):");
+                c =sc.nextInt();}
         }
         
         
